953d97c21d7261660cc2958f6490b9909b595b0d
Pedro Grando <104664138+Grandop@users.noreply.github.com>
2022-05-12 17:35:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+/*function ScrollReveal() {
+  var options = {
+    reveal: function() {}
+  }
+  return options
+}
+
+var options = ScrollReveal()*/
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card`)
