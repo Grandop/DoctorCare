a1186e0fa26a9857de4904a7744020e3860e616f
Pedro Grando <grandop03@gmail.com>
2022-06-11 02:53:42 -0300

    Finalizando o site para Mobile
    Opção de mudança das cores da página, finalizando o footer e a seção de contatos. Implementação do botão back to top.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 800) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -28,12 +44,7 @@ ScrollReveal({
   distance: '30px',
   duration: 700
 }).reveal(`
-#home, 
-#home img, 
-#home .stats, 
-#services,
-#services header,
-#services .card
-#about,
-#about header
-#about .content`)
+#home, #home img, #home .stats, 
+#services,#services header,#services .card,
+#about,#about header, #about .content,
+#contact,#contact .wrapper,#contact .content`)
